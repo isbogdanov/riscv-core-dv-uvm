9ff6e0e5d313412e28e17663332ed064741799e8
Igor Bogdanov <i.bogdanov@ibcico.com>
2025-06-21 15:08:42 -0400

    removing legacy files, configurations and scripts

--- uvm_env/custom_target/rv32i/riscv_core_setting.sv
+++
@@ -1,6 +0,0 @@
-// This file is the central configuration for the riscv-dv test generator.
-// It defines the capabilities of the target processor.
-
-parameter string supported_isa = "RV32IM_Zicsr";
-parameter bit    disable_compressed_instr = 1;
-parameter bit    check_misa_init = 1; 
